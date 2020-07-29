0a4093a495c7531b642fdc0f52042f473ea86dae
ajuvercr <arthur.vercruysse@outlook.com>
2020-07-29 21:10:35 +0200

    add style and fix resize at start

--- frontend/src/webgl/webgl.rs
+++ frontend/src/webgl/webgl.rs
@@ -15,7 +15,7 @@ pub struct WebGl {
     render_loop: Option<Box<dyn Task>>,
     aspect: f32,
 
-    resize_task: ResizeTask,
+    _resize_task: ResizeTask,
 }
 
 pub enum Msg {
@@ -28,8 +28,8 @@ impl Component for WebGl {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        let resize_task = ResizeService::new().register(link.callback(|dim| Msg::Resize(dim)));
-        
+        let _resize_task = ResizeService::new().register(link.callback(|dim| Msg::Resize(dim)));
+
         Self {
             canvas: None,
             gl: None,
@@ -38,15 +38,11 @@ impl Component for WebGl {
             render_loop: None,
             aspect: 1.0,
 
-            resize_task,
+            _resize_task,
         }
     }
 
     fn rendered(&mut self, first_render: bool) {
-        // Once rendered, store references for the canvas and GL context. These can be used for
-        // resizing the rendering area when the window or canvas element are resized, as well as
-        // for making GL calls.
-
         let canvas = self.node_ref.cast::<HtmlCanvasElement>().unwrap();
 
         let gl: GL = canvas
@@ -59,37 +55,31 @@ impl Component for WebGl {
         self.canvas = Some(canvas);
         self.gl = Some(gl);
 
-        // In a more complex use-case, there will be additional WebGL initialization that should be
-        // done here, such as enabling or disabling depth testing, depth functions, face
-        // culling etc.
 
         if first_render {
-            // The callback to request animation frame is passed a time value which can be used for
-            // rendering motion independent of the framerate which may vary.
             let render_frame = self.link.callback(Msg::Render);
             let handle = RenderService::request_animation_frame(render_frame);
-
-            // A reference to the handle must be stored, otherwise it is dropped and the render won't
-            // occur.
             self.render_loop = Some(Box::new(handle));
-        } else {
-            // if self.aspect == 0.0 {
-            //     let parent = self.canvas.as_ref().unwrap().parent_element().unwrap();
-            //     let width = parent.client_width();
-            //     let height = parent.client_height();
-            //     ConsoleService::log(&format!("W: {}, H: {}", width, height));
-            //     self.link.send_message(Msg::Resize(WindowDimensions { width, height }));
-            // }
+
+
+            // Setup size correctly
+            let canvas = self.canvas.as_ref().unwrap();
+            let gl = self.gl.as_ref().expect("GL Context not initialized!");
+
+            let width = canvas.parent_element().unwrap().client_width();
+            let height = canvas.parent_element().unwrap().client_height();
+
+            canvas.set_width(width as u32);
+            canvas.set_height(height as u32);
+            gl.viewport(0, 0, width, height);
+
+            self.aspect = width as f32 / height as f32;
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::Render(timestamp) => {
-                // Render functions are likely to get quite large, so it is good practice to split
-                // it into it's own function rather than keeping it inline in the update match
-                // case. This also allows for updating other UI elements that may be rendered in
-                // the DOM like a framerate counter, or other overlaid textual elements.
                 self.render_gl(timestamp);
             },
             Msg::Resize(WindowDimensions { width, height }) => {
@@ -126,23 +116,13 @@ impl WebGl {
         let vert_code = include_str!("./basic.vert");
         let frag_code = include_str!("./basic.frag");
 
-        // This list of vertices will draw two triangles to cover the entire canvas.
-        // let vertices: Vec<f32> = vec![
-        //     -1.0, -1.0,
-        //     1.0, -1.0,
-        //     -1.0, 1.0,
-        //     -1.0, 1.0,
-        //     1.0, -1.0,
-        //     1.0, 1.0,
-        // ];
 
         let vertices = gen_generalized_spiral(700.0, 3.6);
         // let vertices = gen_sphere_icosahedral(0);
-        // ConsoleService::log(&format!("{:?}", vertices));
 
         let vertex_buffer = gl.create_buffer().unwrap();
-        let verts = js_sys::Float32Array::from(vertices.as_slice());
-        // let verts = unsafe { js_sys::Float32Array::view(vertices.as_slice() )};
+        // let verts = js_sys::Float32Array::from(vertices.as_slice());
+        let verts = unsafe { js_sys::Float32Array::view(vertices.as_slice() )};
 
         gl.bind_buffer(GL::ARRAY_BUFFER, Some(&vertex_buffer));
         gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &verts, GL::STATIC_DRAW);
